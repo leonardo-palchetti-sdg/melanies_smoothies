1a20d5efa750b145519c6cde3b81ee868a44df60
leonardo-palchetti-sdg <108677576+leonardo-palchetti-sdg@users.noreply.github.com>
2026-05-13 22:39:24 +0200

    Integrate fruit nutrition info retrieval
    Added functionality to fetch and display nutrition information for selected fruits from an external API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests 
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -27,9 +28,10 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredient_string)
+      ingredients_string += fruit_chosen + ' '
+      st.subheader(fruit_chosen + ' Nutrition Information')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)  
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -37,5 +39,7 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     #st.write(my_insert_stmt)
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+      session.sql(my_insert_stmt).collect()
+      st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+ 
